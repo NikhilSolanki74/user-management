438c70a033d77f0c7e5f1388e4d0a49e2e194f2b
Nikhil <solankinikhil74@gmail.com>
2024-07-11 18:57:08 +0530

    hidepass feature

--- views/partials/hidePass.js
+++ views/partials/hidePass.js
@@ -1,32 +1,18 @@
  <script>
- let a =true,b = true,actualpass =''; 
 
-let updatePass = () =>{
-     actualpass = document.getElementById('inputPassword').value;
-     document.getElementById('realPassword').value = actualpass;
-     
-}
 
 
 let showPass = ()=>{
-    {/* if(a){
-        actualpass = document.getElementById('inputPassword').value
-        
-         a=false
-    } */}
-    let str =''
-  let size = actualpass.length 
- for(let i =0 ; i<size ;i++){
-  str = str + '*'
- }
-    if(b){
-        document.getElementById('inputPassword').value=str 
-     b=false
+    let ps = document.getElementById('password')
+    if(ps.type == 'text'){
+        document.getElementById('password').type = 'password'
     }else{
-        document.getElementById('inputPassword').value = actualpass
-        b=true
+        document.getElementById('password').type = 'text'
     }
 }
+
+    
+
 
 
 </script>
